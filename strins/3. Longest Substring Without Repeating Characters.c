172bb9d129545cebc43550c7b9d744f5419577e5
nyascla <lavernia.llorens@gmail.com>
2024-10-17 03:13:58 +0200

    new

---
+++ strins/3. Longest Substring Without Repeating Characters.c
@@ -0,0 +1,15 @@
+// Given a string s, 
+// find the length of the longest substring
+// without repeating characters.
+
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d \n", i);
+    }
+    
+    printf("Hola, Mundo!\n");
+    return 0;
+}
