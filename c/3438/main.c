85f72b2bac41ee08fdb40408d1a40c414ef6b6af
Your Name <you@example.com>
2025-02-07 20:09:33 -0500

    a

--- c/3438/main.c
+++ c/3438/main.c
@@ -1,9 +1,45 @@
-#include <stfio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-char* findValidPair(char* s) {  
+int* recount(char* s) {  
+    char *ps = s;
+    int arr[10] = {0};
+
+    while (*ps != '\0'){
+        int n = atoi(*ps);
+
+        arr[n]++;
+    }
+    
+    return arr;
 }
 
-int main(){
-	printf("hola");
-	return 1;
+
+char* findValidPair(char* s) {  
+    char *ps = s;
+    char *pss = s;
+    ps++;
+    
+    while (*ps != '\0'){
+        printf("%c \n",*ps);
+
+        if (*ps != *pss){
+            printf("good %c %c \n", *ps, *pss);
+        }
+        pss++;
+        ps++; 
+    }
+    
+    char *str = "hola";
+    return str;
+}
+
+int main(int argc, char *argv[]) {
+	printf("try: %s\n", argv[1]);
+    
+    char *s = findValidPair(argv[1]);
+
+    printf("result: %s", s);
+    return 1;
+
 }
